da2f19dd1baf3aeb68448dec26b28d52c68484e1
yeef93 <38846337+yeef93@users.noreply.github.com>
2024-09-03 21:04:29 +0700

    fix:comment set-password

--- app/set-password/page.tsx
+++ app/set-password/page.tsx
@@ -1,44 +1,44 @@
-"use client"
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
+// "use client"
+// import { useState } from 'react';
+// import { useRouter } from 'next/navigation';
 
-export default function SetPassword() {
-  const [password, setPassword] = useState('');
-  const router = useRouter();
-  const { token } = router.query;
+// export default function SetPassword() {
+//   const [password, setPassword] = useState('');
+//   const router = useRouter();
+//   const { token } = router.query;
 
-  const handleSubmit = async (e:any) => {
-    e.preventDefault();
-    try {
-      const response = await fetch('/api/auth/set-password', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ token, password }),
-      });
+//   const handleSubmit = async (e:any) => {
+//     e.preventDefault();
+//     try {
+//       const response = await fetch('/api/auth/set-password', {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({ token, password }),
+//       });
 
-      if (response.ok) {
-        alert('Password set successfully');
-        router.push('/login');
-      } else {
-        console.error('Password setup error:', response.statusText);
-      }
-    } catch (error) {
-      console.error('Password setup error:', error);
-    }
-  };
+//       if (response.ok) {
+//         alert('Password set successfully');
+//         router.push('/login');
+//       } else {
+//         console.error('Password setup error:', response.statusText);
+//       }
+//     } catch (error) {
+//       console.error('Password setup error:', error);
+//     }
+//   };
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        placeholder="Enter new password"
-        required
-      />
-      <button type="submit">Set Password</button>
-    </form>
-  );
-}
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <input
+//         type="password"
+//         value={password}
+//         onChange={(e) => setPassword(e.target.value)}
+//         placeholder="Enter new password"
+//         required
+//       />
+//       <button type="submit">Set Password</button>
+//     </form>
+//   );
+// }
